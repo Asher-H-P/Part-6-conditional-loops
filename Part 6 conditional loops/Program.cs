65e096d458ed88765d66b3a2fb4403b6cd89b9ae
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-26 10:59:16 -0400

    Numbers & batman.

--- Part 6 conditional loops/Program.cs
+++ Part 6 conditional loops/Program.cs
@@ -10,7 +10,20 @@ namespace Part_6_conditional_loops
     {
         static void Main(string[] args)
         {
-
+            int i = 0;
+            string answer = "";
+            while (i <= 10)
+            {
+                Console.Write(i);
+                i = i + 2;
+            }
+            Console.WriteLine();
+            while (answer != "batman")
+            {
+                Console.WriteLine("What is Bruce Wayne's alter ego?");
+                answer = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine("CORRECT!");
         }
     }
 }
